f0a3899d748d45611d257b21e0002e548ae45b94
Dan Rouse <dan@rouse.house>
2018-02-09 14:05:35 -0500

    add GraphQL::BaseType.define_includable_connection

--- lib/graphql_includable.rb
+++ lib/graphql_includable.rb
@@ -5,3 +5,15 @@ require 'graphql_includable/edge'
 GraphQL::Field.accepts_definitions(
   includes: GraphQL::Define.assign_metadata_key(:includes)
 )
+
+module GraphQL
+  class BaseType
+    def define_includable_connection(**kwargs, &block)
+      define_connection(
+        edge_class: GraphQLIncludable::Edge,
+        **kwargs,
+        &block
+      )
+    end
+  end
+end

78bc19b38b151a9c732e29927caa32047bc49e33
Dan Rouse <dan@rouse.house>
2018-02-09 14:05:50 -0500

    update edge resolution to support has_many through multiple models

--- lib/graphql_includable/edge.rb
+++ lib/graphql_includable/edge.rb
@@ -1,30 +1,55 @@
 module GraphQLIncludable
   class Edge < GraphQL::Relay::Edge
-    def method_missing(sym, *args, &block)
-      get_single_record(sym) || super
+    def edge
+      join_chain = joins_along_edge
+      edge_class_name = join_chain.shift
+      edge_class = str_to_class(edge_class_name)
+
+      root_node = { class_to_str(parent.class).to_s.singularize => parent }
+      terminal_node = { class_to_str(node.class).singularize => node }
+      join_chain.reverse.each do |rel_name|
+        terminal_node = { rel_name.to_s.pluralize => terminal_node }
+      end
+      search_hash = root_node.merge(terminal_node)
+
+      @edge ||= edge_class.includes(*join_chain.map { |s| s.to_s.singularize }).find_by(search_hash)
     end
 
-    def respond_to_missing?(sym, include_private = false)
-      !get_association(sym).nil? || super
+    def method_missing(method_name, *args, &block)
+      if edge.respond_to?(method_name)
+        edge.send(method_name, *args, &block)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing(method_name, include_private = false)
+      edge.respond_to?(method_name) || super
     end
 
     private
 
-    def get_association(sym)
-      association_name = sym.to_s.pluralize.to_sym
-      parent.class.reflect_on_association(association_name)
+    def str_to_class(str)
+      str.to_s.singularize.camelize.constantize
+    rescue
     end
 
-    def get_single_record(sym)
-      association = get_association(sym)
-      return unless association
-      target_class = node.class
-      target_association_name = target_class.name.singularize.downcase
-      node_id = node[target_class.primary_key]
-      foreign_key = association.klass.reflect_on_association(target_association_name).foreign_key
-      parent.method(association.name).call.find do |record|
-        record[foreign_key] == node_id
+    def class_to_str(klass)
+      klass.name.pluralize.downcase
+    end
+
+    def joins_along_edge
+      join_chain = []
+      starting_class = parent.class
+      node_relationship_name = class_to_str(node.class)
+      while starting_class
+        reflection = starting_class.reflect_on_association(node_relationship_name)
+        association_name = reflection&.options&.try(:[], :through)
+        join_chain << association_name if association_name
+        starting_class = str_to_class(association_name)
+        node_relationship_name = node_relationship_name.singularize
       end
+      join_chain
     end
   end
 end

5356c5ccb2355e4bbf22d138ac48168194a97753
Jordan Hamill <jordan@squarefoot.com>
2019-09-04 11:51:58 +0100

    Allow starting includes resolution from any point beyond ctx.irep_node

--- lib/graphql_includable/new/gql_includable.rb
+++ lib/graphql_includable/new/gql_includable.rb
@@ -7,18 +7,31 @@ require_relative 'relay/instrumentation'
 
 module GraphQLIncludable
   module New
-    def self.includes(ctx)
+    def self.includes(ctx, starting_at: nil)
       ActiveSupport::Notifications.instrument('graphql_includable.includes') do |instrument|
         instrument[:operation_name] = ctx.query&.operation_name
         instrument[:field_name] = ctx.irep_node.name
+        instrument[:starting_at] = starting_at
 
         includes = Includes.new(nil)
         resolver = Resolver.new(ctx)
-        resolver.includes_for_node(ctx.irep_node, includes)
+
+        node = ctx.irep_node
+        node = node_for_path(node, starting_at) if starting_at.present?
+        byebug
+        raise ArgumentError, 'Invalid starting_at path' unless node.present?
+
+        resolver.includes_for_node(node, includes)
         generated_includes = includes.active_record_includes
+
         instrument[:includes] = generated_includes
         generated_includes
       end
+    end
+
+    def self.node_for_path(node, path_into_query)
+      children = node.scoped_children[node.return_type.unwrap]&.with_indifferent_access || {}
+      children.dig(path_into_query)
     end
   end
 end

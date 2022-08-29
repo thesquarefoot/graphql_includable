cded882d3779ba8f9b98b26fbbd13ab2fc675d96
sforsell <sofia@squarefoot.com>
2022-08-29 11:30:09 +0200

    wip

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'activerecord'
-gem 'activesupport'
-gem 'graphql', '1.7.7'
+gem 'activerecord', '~> 5.1.0'
+gem 'activesupport', '~> 5.1.0'
+gem 'graphql', '1.12.24'
 
 group :test do
   gem 'rspec'
@@ -16,4 +16,5 @@ group :development do
   gem 'guard-rspec'
   gem 'rspec-instrumentation-matcher'
   gem 'sqlite3', '~> 1.3.6'
+  gem 'rake'
 end
--- spec/graphql_schema.rb
+++ spec/graphql_schema.rb
@@ -1,33 +1,80 @@
 GraphQL::Relay::ConnectionType.default_nodes_field = true
 
-UserType = GraphQL::ObjectType.define do
-  name 'User'
-  field :name, !types.String
-  field :email, !types.String
-  field :array_clients, !types[!ClientType], property: :clients, includes: :clients
-  connection :clients, ClientType.define_connection do
-    includes ->() { nodes(:clients) }
+class BaseObject < GraphQL::Schema::Object
+  connection_type_class(GraphQL::Types::Relay::BaseConnection)
 
-    resolve ->(user, _args, _ctx) do
-      raise 'Missing includes' unless user.association(:clients).loaded?
-      user.clients
-    end
+  def self.define_connection_with_fetched_edge(**kwargs, &block)
+    GraphQLIncludable::Relay::EdgeWithNodeConnectionType.create_type(
+      self,
+      **kwargs,
+      &block
+    )
   end
 end
 
-LocationType = GraphQL::ObjectType.define do
-  name 'Location'
-  field :name, !types.String
+class IncludableField < GraphQL::Schema::Field
+  # maybe just need to wrap in a accepts_definition?
+  def initialize(*args, includes: nil, **kwargs, &block)
+    @includes = includes
+    super(*args, **kwargs, &block)
+  end
+
+  def includes(*args)
+    args
+  end
+  # def resolve(type, args, ctx)
+  #   return super unless @includess
+
+  #   BatchLoader::GraphQL.for(type).batch(key: self) do |records, loader|
+  #     ActiveRecord::Associations::Preloader.new.preload(records.map(&:object), @includes)
+  #     records.each { |r| loader.call(r, super(r, args, ctx)) }
+  #   end
+  # end
 end
 
-TaskType = GraphQL::ObjectType.define do
-  name 'Task'
-  field :name, !types.String
-  field :location, !LocationType, includes: :location
+# Define how to get from an edge Active Record model to the node Active Record model
+IncludableField.accepts_definition(:connection_properties)
+
+# Define a resolver for connection edges records
+IncludableField.accepts_definition(:resolve_edges)
+
+# # Define a resolver for connection nodes records
+IncludableField.accepts_definition(:resolve_nodes)
+
+# # Internally used to mark a connection type that has a fetched edge
+IncludableField.accepts_definition(:_includable_connection_marker)
+
+class UserType < BaseObject
+  graphql_name 'User'
+  field :name, String, null: false
+  field :email, String, null: false
+  # field :array_clients, [ClientType, { null: false }], null: false, method: :clients, includes: :clients
+  # field :clients, ClientType.define_connection, null: true, connection: true do
+  #   includes ->(_args, _ctx) { nodes(:clients) }
+  # end
+
+  def clients
+    raise 'Missing includes' unless object.association(:clients).loaded?
+
+    object.clients
+  end
+end
+
+class LocationType < BaseObject
+  graphql_name 'Location'
+  field :name, String, null: false
+end
+
+class TaskType < BaseObject
+  graphql_name 'Task'
+  field_class IncludableField
+
+  field :name, String, null: false
+  field :location, LocationType, null: false, includes: :location
 end
 
 ClientTaskEdgeType = TaskType.define_edge do
-  field :completed, !types.Boolean
+  field :completed, Boolean, null: false
 end
 
 OverFetchedConnectionType = TaskType.define_connection_with_fetched_edge(edge_type: ClientTaskEdgeType) do
@@ -38,17 +85,19 @@ NestedQueryConnectionType = TaskType.define_connection_with_fetched_edge(edge_ty
   name 'NestedQueryConnection'
 end
 
-ClientType = GraphQL::ObjectType.define do
-  name 'Client'
-  field :name, !types.String
-  field :user, !UserType, includes: :user
-  field :array_tasks, !types[!TaskType], property: :tasks do
+class ClientType < BaseObject
+  graphql_name 'Client'
+  field_class IncludableField
+
+  field :name, String, null: false
+  field :user, UserType, null: false, includes: :user
+  field :array_tasks, [TaskType, { null: false }], null: false, method: :tasks do
     includes ->() do
       path(:client_tasks, :task)
     end
   end
 
-  connection :tasks, TaskType.define_connection_with_fetched_edge(edge_type: ClientTaskEdgeType) do
+  field :tasks, TaskType.define_connection_with_fetched_edge(edge_type: ClientTaskEdgeType), null: true, connection: true do
     connection_properties(nodes: :tasks, edges: :client_tasks, edge_to_node: :task)
 
     includes ->() do
@@ -60,7 +109,7 @@ ClientType = GraphQL::ObjectType.define do
     end
   end
 
-  connection :over_fetched, OverFetchedConnectionType do
+  field :over_fetched, OverFetchedConnectionType, null: true, connection: true do
     connection_properties(edge_to_node: :task)
 
     includes ->() do
@@ -80,7 +129,7 @@ ClientType = GraphQL::ObjectType.define do
     end
   end
 
-  connection :nested_query, NestedQueryConnectionType do
+  field :nested_query, NestedQueryConnectionType, null: true, connection: true do
     connection_properties(edge_to_node: :task)
 
     includes ->() do
@@ -96,36 +145,38 @@ ClientType = GraphQL::ObjectType.define do
     end
   end
 
-  connection :new_chain, TaskType.define_connection(edge_type: ClientTaskEdgeType) { name 'NewChainConnection' } do
-    argument :continue_includes, !types.Boolean
+  field :new_chain, TaskType.define_connection(edge_type: ClientTaskEdgeType) { name 'NewChainConnection' }, null: true, connection: true do
+    argument :continue_includes, Boolean, required: true
     includes ->(args, _ctx) do
       return unless args[:continue_includes]
+
       nodes(:tasks)
     end
 
     resolve ->(client, args, ctx) do
       return client.tasks if args[:continue_includes]
+
       Task.includes(GraphQLIncludable.includes(ctx)).all
     end
   end
 end
 
-GraphQLSchema = GraphQL::Schema.define(
-  query: GraphQL::ObjectType.define do
-    name 'BaseQuery'
-    field :users, !types[!UserType] do
-      resolve ->(_obj, _args, ctx) do
-        User.includes(GraphQLIncludable.includes(ctx)).all
-      end
-    end
+class BaseQuery < GraphQL::ObjectType
+  name 'BaseQuery'
+  field :users, [UserType, { null: false }], null: false
+  field :clients, [ClientType, { null: false }], null: false
 
-    field :clients, !types[!ClientType] do
-      resolve ->(_obj, _args, ctx) do
-        Client.includes(GraphQLIncludable.includes(ctx)).all
-      end
-    end
+  def users(_obj, _args, ctx)
+    User.includes(GraphQLIncludable.includes(ctx)).all
   end
-) do
+
+  def clients(_obj, _args, ctx)
+    Client.includes(GraphQLIncludable.includes(ctx)).all
+  end
+end
+
+class GraphQLSchema < GraphQL::Schema
+  query BaseQuery
   instrument(:field, GraphQLIncludable::Relay::Instrumentation.new)
   resolve_type ->(_type, obj, _ctx) { Object.const_get("#{obj.class}Type") }
 end
